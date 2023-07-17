ddd1dc65e778e78d7de08b67259f37ac5edf36cc
anwholesquare <anwholesquare@gmail.com>
2023-07-17 23:36:27 +0600

    First Commit

---
+++ app/src/main/java/com/khandokeranan/austopen/VacantAcitivity3.java
@@ -0,0 +1,14 @@
+package com.khandokeranan.austopen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VacantAcitivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacant_acitivity3);
+    }
+}
