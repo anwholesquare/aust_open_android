2bd02a5d6bd12d21a9dddded51aaff37001a1d21
anwholesquare <anwholesquare@gmail.com>
2023-07-18 00:58:20 +0600

    Button Click Added on MainActivity

--- app/src/main/java/com/khandokeranan/austopen/MainActivity.java
+++ app/src/main/java/com/khandokeranan/austopen/MainActivity.java
@@ -1,8 +1,12 @@
 package com.khandokeranan.austopen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView vacantBtn = (CardView) findViewById(R.id.vacantBtn);
+        vacantBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vc1 = new Intent(getApplicationContext(), VacantAcitivity1.class);
+                startActivity(vc1);
+            }
+        });
+
+
+        CardView noticeBtn = (CardView) findViewById(R.id.noticeBtn);
+        noticeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vc1 = new Intent(getApplicationContext(), VacantAcitivity1.class);
+                startActivity(vc1);
+            }
+        });
+
+
+        CardView infoBtn = (CardView) findViewById(R.id.infoBtn);
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vc1 = new Intent(getApplicationContext(), VacantAcitivity1.class);
+                startActivity(vc1);
+            }
+        });
+
+
+        CardView routineBtn = (CardView) findViewById(R.id.routineBtn);
+        routineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vc1 = new Intent(getApplicationContext(), VacantAcitivity1.class);
+                startActivity(vc1);
+            }
+        });
+
+
+
+        CardView busBtn = (CardView) findViewById(R.id.busBtn);
+        busBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vc1 = new Intent(getApplicationContext(), VacantAcitivity1.class);
+                startActivity(vc1);
+            }
+        });
+
+
+
+        CardView accBtn = (CardView) findViewById(R.id.accBtn);
+        accBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vc1 = new Intent(getApplicationContext(), VacantAcitivity1.class);
+                startActivity(vc1);
+            }
+        });
+
+
 
 
 
