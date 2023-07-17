63525107d7fd704a5124b4e3877a1326ad88a50b
anwholesquare <anwholesquare@gmail.com>
2023-07-18 05:08:44 +0600

    Dropdown system Added

--- app/src/main/java/com/khandokeranan/austopen/VacantAcitivity2.java
+++ app/src/main/java/com/khandokeranan/austopen/VacantAcitivity2.java
@@ -3,6 +3,11 @@ package com.khandokeranan.austopen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class VacantAcitivity2 extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class VacantAcitivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacant_acitivity2);
-    }
+
+        String[] floors = new String[] {"1ST FLOOR", "2ND FLOOR", "3RD FLOOR", "4TH FLOOR", "5TH FLOOR", "6TH FLOOR", "7TH FLOOR", "8TH FLOOR", "9TH FLOOR"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.dropdown_items, floors);
+
+        AutoCompleteTextView floorText = (AutoCompleteTextView) findViewById(R.id.FloorDropDown);
+        floorText.setAdapter(arrayAdapter);
+        floorText.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), floorText.getText().toString(), Toast.LENGTH_SHORT);
+            }
+        });
+     }
 }
