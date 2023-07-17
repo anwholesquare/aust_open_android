1f5439e1dc6c172bf3115c97c14c5cc7f717cdf3
iHabibullahBahar <habibhbh4u@gmail.com>
2023-07-18 03:56:45 +0600

    Vacant room added

--- app/src/main/java/com/khandokeranan/austopen/VacantAcitivity1.java
+++ app/src/main/java/com/khandokeranan/austopen/VacantAcitivity1.java
@@ -3,6 +3,9 @@ package com.khandokeranan.austopen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.content.Intent;
+import android.view.View;
 
 public class VacantAcitivity1 extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class VacantAcitivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacant_acitivity1);
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.f9thBtn);
+        frameLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vc2 = new Intent(getApplicationContext(), VacantAcitivity2.class);
+                startActivity(vc2);
+            }
+        });
     }
 }
