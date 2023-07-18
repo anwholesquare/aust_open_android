13515abe682107bec26148ba5bd082f2e9d1002d
anwholesquare <anwholesquare@gmail.com>
2023-07-18 10:00:06 +0600

    PDFView Added

--- app/src/main/java/com/khandokeranan/austopen/NoticeActivity2.java
+++ app/src/main/java/com/khandokeranan/austopen/NoticeActivity2.java
@@ -1,8 +1,12 @@
 package com.khandokeranan.austopen;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 public class NoticeActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class NoticeActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice2);
+
+        WebView webView = (WebView) findViewById(R.id.pdfWebView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://drive.google.com/file/d/1J4T1pfnbp4LiaFk_ZYT1G9We_sgJu9os/view");
+
+
+
     }
+
+
 }
