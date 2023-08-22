63480375d3c859f7ebf944330fd80a3c71fecdf7
iHabibullahBahar <habibhbh4u@gmail.com>
2023-08-22 08:11:11 +0600

    Final Commit

--- app/src/main/java/com/khandokeranan/austopen/BusActivity.java
+++ app/src/main/java/com/khandokeranan/austopen/BusActivity.java
@@ -1,7 +1,10 @@
 package com.khandokeranan.austopen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class BusActivity extends AppCompatActivity {
@@ -10,5 +13,26 @@ public class BusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus);
+
+        CardView surmaBtn = (CardView) findViewById(R.id.surmaBtn);
+        surmaBtn.setOnClickListener(v -> {
+            String url = "https://m.me/j/AbY8Zdps6C0yu6Cc/";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+        CardView jamunaBtn = (CardView) findViewById(R.id.jamunaBtn);
+        jamunaBtn.setOnClickListener(v -> {
+            String url = "https://www.messenger.com/t/4787755037945490";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+        CardView karnafuliBtn = (CardView) findViewById(R.id.kornofuliBtn);
+        karnafuliBtn.setOnClickListener(v -> {
+            String url = "https://www.messenger.com/t/2706778839410669";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+
+
     }
 }
